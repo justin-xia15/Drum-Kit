92f538d596205633ddb6be4134caf6f827c0e9ae
Justin Xia <justinxia9@gmail.com>
2021-08-18 15:13:17 -0400

    Add key animations

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-window.addEventListener("keydown", function(event) {
+const keys = document.querySelectorAll(".keys");
+
+function playSound(event) {
 	  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
 	  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 	  if(!audio) {
 		      return;
 		    } // stops the function from running
 	  audio.play();
-
+	  key.classList.add("playing");
+	  setTimeout(function() {
+		      key.classList.remove("playing");
+		    }, 100);
 	  audio.currentTime = 0; // rewinds to the start
-});
+}
 
+window.addEventListener("keydown", playSound);
+
+
+	  
